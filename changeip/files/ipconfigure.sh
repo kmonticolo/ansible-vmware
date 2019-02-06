e1374634bd17683f7271b771693420637387a80a
kmonticolo <kmonticolo>
2019-02-06 15:09:59 +0100

    add changeip role

---
+++ changeip/files/ipconfigure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NETWORK=10.48.77
+IFACE=$(ifconfig -a|grep ^ens | cut -f1 -d: | head -1)
+
+MAC=$(ifconfig $IFACE |grep ether | awk '{print $2}')
+
+[[ $MAC ]] && sed -i "s/^HWADDR=.*/HWADDR=$MAC/" /etc/sysconfig/network-scripts/ifcfg-$IFACE
+
+flag=0 ; for i in `seq 13 250`;do arping -w2 -c1 -I $IFACE "$NETWORK".$i >/dev/null || flag=$?; if [ "$flag" -eq "1" ]; then export IP="$NETWORK".$i; echo $IP; break;fi ;done
+
+[[ $IP ]] && sed -i "s/^IPADDR=.*/IPADDR=\"$IP\"/" /etc/sysconfig/network-scripts/ifcfg-$IFACE
