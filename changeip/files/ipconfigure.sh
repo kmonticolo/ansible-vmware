9b020e0e5fe5a82fa8f714d21aff7224168931de
kmonticolo <kmonticolo>
2019-02-19 17:01:19 +0100

    bump number

--- changeip/files/ipconfigure.sh
+++ changeip/files/ipconfigure.sh
@@ -7,7 +7,7 @@ MAC=$(ifconfig $IFACE |grep ether | awk '{print $2}')
 
 [[ $(grep -q $MAC /etc/sysconfig/network-scripts/ifcfg-$IFACE) ]] || sed -i "s/^HWADDR=.*/HWADDR=$MAC/" /etc/sysconfig/network-scripts/ifcfg-$IFACE
 
-flag=0 ; for i in `seq 13 250`;do arping -w2 -c1 -I $IFACE "$NETWORK".$i >/dev/null || flag=$?; if [ "$flag" -eq "1" ]; then export IP="$NETWORK".$i; echo $IP; break;fi ;done
+flag=0 ; for i in `seq 31 250`;do arping -w2 -c1 -I $IFACE "$NETWORK".$i >/dev/null || flag=$?; if [ "$flag" -eq "1" ]; then export IP="$NETWORK".$i; echo $IP; break;fi ;done
 
 [[ "$1" == "check" ]] && exit 0
 [[ $IP ]] && sed -i "s/^IPADDR=.*/IPADDR=\"$IP\"/" /etc/sysconfig/network-scripts/ifcfg-$IFACE
