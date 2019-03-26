a353ed7deb8d8110638291ef19a92070a89176aa
Kamil Monticolo <kmonticolo@dcclabs.tv>
2019-03-26 10:02:30 +0100

    auto calculate seq start sequence

--- changeip/files/ipconfigure.sh
+++ changeip/files/ipconfigure.sh
@@ -6,8 +6,9 @@ IFACE=$(ifconfig -a|grep ^ens | cut -f1 -d: | head -1)
 MAC=$(ifconfig $IFACE |grep ether | awk '{print $2}')
 
 [[ $(grep -q $MAC /etc/sysconfig/network-scripts/ifcfg-$IFACE) ]] || sed -i "s/^HWADDR=.*/HWADDR=$MAC/" /etc/sysconfig/network-scripts/ifcfg-$IFACE
-
-flag=0 ; for i in `seq 31 250`;do arping -w2 -c1 -I $IFACE "$NETWORK".$i >/dev/null || flag=$?; if [ "$flag" -eq "1" ]; then export IP="$NETWORK".$i; echo $IP; break;fi ;done
+START=$(ip route get 1 | awk '{print $NF;exit}'|sed 's/^.*\.//')
+START=$(($START + 1))
+flag=0 ; for i in `seq $START 250`;do arping -w2 -c1 -I $IFACE "$NETWORK".$i >/dev/null || flag=$?; if [ "$flag" -eq "1" ]; then export IP="$NETWORK".$i; echo $IP; break;fi ;done
 
 [[ "$1" == "check" ]] && exit 0
 [[ $IP ]] && sed -i "s/^IPADDR=.*/IPADDR=\"$IP\"/" /etc/sysconfig/network-scripts/ifcfg-$IFACE
