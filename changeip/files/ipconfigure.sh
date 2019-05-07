6cc991c00ae24d2f62c7e6c1b92029b3dc57031c
kmonticolo <kmonticolo>
2019-05-07 12:01:51 +0200

    add centos6 support

--- changeip/files/ipconfigure.sh
+++ changeip/files/ipconfigure.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 NETWORK=10.48.77
-IFACE=$(ifconfig -a|grep ^ens | cut -f1 -d: | head -1)
+#IFACE=$(ifconfig -a|grep ^ens | cut -f1 -d: | head -1)
+IFACE=$(ip -o link show | grep -E '(ens|eth)' | awk '{ print $2 }' | sed 's/://')
 
-MAC=$(ifconfig $IFACE |grep ether | awk '{print $2}')
+
+grep -q "release 6" /etc/centos-release && MAC=$(ifconfig $IFACE |grep HWaddr | awk '{print $5}')
+grep -q "release 7" /etc/centos-release && MAC=$(ifconfig $IFACE |grep ether | awk '{print $2}')
+
 
 [[ $(grep -q $MAC /etc/sysconfig/network-scripts/ifcfg-$IFACE) ]] || sed -i "s/^HWADDR=.*/HWADDR=$MAC/" /etc/sysconfig/network-scripts/ifcfg-$IFACE
 
@@ -11,3 +15,5 @@ flag=0 ; for i in `seq 31 250`;do arping -w2 -c1 -I $IFACE "$NETWORK".$i >/dev/n
 
 [[ "$1" == "check" ]] && exit 0
 [[ $IP ]] && sed -i "s/^IPADDR=.*/IPADDR=\"$IP\"/" /etc/sysconfig/network-scripts/ifcfg-$IFACE
+MAC=$(ifconfig $IFACE |grep HWaddr | awk '{print $5}')
+
