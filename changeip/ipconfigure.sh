861c73d672fddc81354d2cffa91ec5e86a24db77
kmonticolo <kmonticolo>
2019-05-08 08:07:53 +0200

    add centos6 support

---
+++ changeip/ipconfigure.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+NETWORK=10.48.77
+#IFACE=$(ifconfig -a|grep ^ens | cut -f1 -d: | head -1)
+IFACE=$(ip -o link show | grep -E '(ens|eth)' | awk '{ print $2 }' | sed 's/://')
+
+
+grep -q "release 6" /etc/centos-release && MAC=$(ifconfig $IFACE |grep HWaddr | awk '{print $5}')
+grep -q "release 7" /etc/centos-release && MAC=$(ifconfig $IFACE |grep ether | awk '{print $2}')
+
+
+echo > /etc/udev/rules.d/70-persistent-net.rules 
+udevadm trigger
+
+IFACECFG=$(find /etc/sysconfig/network-scripts/ -name "ifcfg-*"  | head -n 1)
+[[ -f /etc/sysconfig/network-scripts/ifcfg-$IFACE ]] || cp $IFACECFG /etc/sysconfig/network-scripts/ifcfg-$IFACE 
+
+
+[[ $(grep -q $MAC /etc/sysconfig/network-scripts/ifcfg-$IFACE) ]] || sed -i "s/^HWADDR=.*/HWADDR=$MAC/" /etc/sysconfig/network-scripts/ifcfg-$IFACE
+
+flag=0 ; for i in `seq 31 250`;do arping -w2 -c1 -I $IFACE "$NETWORK".$i >/dev/null || flag=$?; if [ "$flag" -eq "1" ]; then export IP="$NETWORK".$i; echo $IP; break;fi ;done
+
+[[ "$1" == "check" ]] && exit 0
+[[ $IP ]] && sed -i "s/^IPADDR=.*/IPADDR=\"$IP\"/" /etc/sysconfig/network-scripts/ifcfg-$IFACE
+MAC=$(ifconfig $IFACE |grep HWaddr | awk '{print $5}')
+
