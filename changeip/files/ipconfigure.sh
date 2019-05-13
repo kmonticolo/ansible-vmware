a56826c600d77ef39df566cbaea1b5288099dafb
Kamil Monticolo <kamil.monticolo>
2019-05-13 14:36:12 +0200

    fix ipconfigure.sh

--- changeip/files/ipconfigure.sh
+++ changeip/files/ipconfigure.sh
@@ -5,9 +5,11 @@ IFACE=$(ifconfig -a|grep ^ens | cut -f1 -d: | head -1)
 
 MAC=$(ifconfig $IFACE |grep ether | awk '{print $2}')
 
-[[ $MAC ]] && sed -i "s/^HWADDR=.*/HWADDR=$MAC/" /etc/sysconfig/network-scripts/ifcfg-$IFACE
+[[ $(grep -q $MAC /etc/sysconfig/network-scripts/ifcfg-$IFACE) ]] || sed -i "s/^HWADDR=.*/HWADDR=$MAC/" /etc/sysconfig/network-scripts/ifcfg-$IFACE
+START=$(ip route get 1 | awk '{print $NF;exit}'|sed 's/^.*\.//')
+START=$(($START + 1))
+flag=0 ; for i in `seq $START 250`;do arping -w2 -c1 -I $IFACE "$NETWORK".$i >/dev/null || flag=$?; if [ "$flag" -eq "1" ]; then export IP="$NETWORK".$i; echo $IP; break;fi ;done
 
-flag=0 ; for i in `seq 13 250`;do arping -w2 -c1 -I $IFACE "$NETWORK".$i >/dev/null || flag=$?; if [ "$flag" -eq "1" ]; then export IP="$NETWORK".$i; echo $IP; break;fi ;done
-
+[[ "$1" == "check" ]] && exit 0
 [[ $IP ]] && sed -i "s/^IPADDR=.*/IPADDR=\"$IP\"/" /etc/sysconfig/network-scripts/ifcfg-$IFACE
 
