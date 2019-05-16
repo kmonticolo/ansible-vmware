4e5dd82bf3a063d9467f3a9827a4a98399ce6445
Kamil Monticolo <kamil.monticolo>
2019-05-16 14:29:58 +0200

    use ip intead of ifconfig

--- changeip/files/ipconfigure.sh
+++ changeip/files/ipconfigure.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 NETWORK=10.48.77
-IFACE=$(ifconfig -a|grep ^ens | cut -f1 -d: | head -1)
+IFACE=$(ip a | grep ens | cut -f2 -d ' ' | sed 's/\://')
 
-MAC=$(ifconfig $IFACE |grep ether | awk '{print $2}')
 
-[[ $(grep -q $MAC /etc/sysconfig/network-scripts/ifcfg-$IFACE) ]] || sed -i "s/^HWADDR=.*/HWADDR=$MAC/" /etc/sysconfig/network-scripts/ifcfg-$IFACE
+MAC=$(ip a s $IFACE | grep ether | awk '{print $2}')
+
+[[ $(grep -q $MAC /etc/sysconfig/network-scripts/ifcfg-$IFACE) ]] || sed -i "s/^HWADDR=.*/HWADDR=$MAC/" /etc/sysconfig/network-scripts/ifcfg-$IFACE >/dev/null 2>&1
 START=$(ip route get 1 | awk '{print $NF;exit}'|sed 's/^.*\.//')
 START=$(($START + 1))
 flag=0 ; for i in `seq $START 250`;do arping -w2 -c1 -I $IFACE "$NETWORK".$i >/dev/null || flag=$?; if [ "$flag" -eq "1" ]; then export IP="$NETWORK".$i; echo $IP; break;fi ;done
